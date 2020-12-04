2cf93da4d865971ef9d70a1a0561c2b25027a528
raiders032 <nameks@naver.com>
2020-12-04 17:26:41 +0900

    Initial commit

---
+++ src/main/java/com/youngthree/rolebasedapplicationmanagement/RoleBasedApplicationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.youngthree.rolebasedapplicationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedApplicationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleBasedApplicationManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youngthree/rolebasedapplicationmanagement/RoleBasedApplicationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngthree.rolebasedapplicationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedApplicationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

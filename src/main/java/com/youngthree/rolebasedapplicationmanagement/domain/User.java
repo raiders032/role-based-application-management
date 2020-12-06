8bdca5b30952932a91b5151db4e511e61dead8d3
raiders032 <nameks@naver.com>
2020-12-06 16:22:28 +0900

    create user domain

---
+++ src/main/java/com/youngthree/rolebasedapplicationmanagement/domain/User.java
@@ -0,0 +1,36 @@
+package com.youngthree.rolebasedapplicationmanagement.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private String[] roles;
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+}
